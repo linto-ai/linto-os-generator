6ba27acc0375b5503ba657f3212b94ed97a063e8
Damien LAINÉ <damien.laine@gmail.com>
2019-11-05 16:12:22 +0100

    Waitforsoundcard multilingual closes #2

--- base/root_part/usr/local/bin/waitforsoundcard.sh
+++ base/root_part/usr/local/bin/waitforsoundcard.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #PATH=/usr/bin
 while [ "$x" == "" ]; do
- l=`/usr/bin/aplay -l | grep périphérique | wc -l`
- if [ "$l" != "0" ]; then
-   echo Found sound cards
-   x="done"
-/bin/bash /usr/local/bin/defaultvolume.sh
- else
-  sleep 2
- fi
+  l=$(/usr/bin/aplay -L | grep hw:CARD | wc -l)
+  if [ "$l" != "0" ]; then
+    echo Found sound cards
+    x="done"
+    /bin/bash /usr/local/bin/defaultvolume.sh
+  else
+    sleep 2
+  fi
 done
